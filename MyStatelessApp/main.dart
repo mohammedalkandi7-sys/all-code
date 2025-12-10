6d2b061b45ac41b402c3350ab7b72828bb6f1e5e
mohammedalkandi7-sys <mohammedalkandi7@gmail.com>
2025-12-10 21:12:08 +0300

    Add files via upload

---
+++ MyStatelessApp/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyStatelessApp());
+}
+
+class MyStatelessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stateless Widget Example'),
+        ),
+        body: Center(
+          child: MyCustomWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyCustomWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Hello, I am a Stateless Widget!',
+      style: TextStyle(fontSize: 24),
+    );
+  }
+}
